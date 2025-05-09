5f10e16d274b0c771ef4c8ace3a4a7f1868b09b6
DHRUVVERMA123 <dhruvmac1234@gmail.com>
2025-05-09 09:35:19 +0530

    Imagekit-auth : api

--- app/api/imagekit-auth/route.ts
+++ app/api/imagekit-auth/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import ImageKit from "imagekit";
 
 const imagekit = new ImageKit({
-  publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KE || ""!,
+  publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY || ""!,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY || "",
   urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT || "",
 });
